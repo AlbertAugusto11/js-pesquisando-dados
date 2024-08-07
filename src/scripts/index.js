9fa9b600279966b9608caee08618ff456401631d
Albert Augusto <augustothelema@gmail.com>
2024-08-06 21:25:49 -0300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 function procuraSobremesa(sobremesas, valorDoInput) {
   /* Desenvolva sua lógica a partir aqui */
+  let x = sobremesas.filter(element =>{
+    return element.nome.toLowerCase().includes(valorDoInput.toLowerCase())
+  })
+  /* É necessário retornar uma lista de produtos */
+  return x; 
 
-  return; /* É necessário retornar uma lista de produtos */
 }
